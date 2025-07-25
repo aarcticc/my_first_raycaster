8549dab55b413feccc8682ee576f359d5c90035d
aarcticc <hcf2509@proton.me>
2025-07-25 13:00:18 +0200

    removed the python mapmaker

--- mapmaker/src/utils/constants.py
+++
@@ -1,9 +0,0 @@
-# File: /mapmaker/src/utils/constants.py
-
-TEXTURE_WIDTH = 64
-TEXTURE_HEIGHT = 64
-DEFAULT_MAP_WIDTH = 20
-DEFAULT_MAP_HEIGHT = 20
-MAX_TEXTURES = 4
-WALL_TEXTURES = ["assets/wall1.png", "assets/wall2.png", "assets/wall3.png", "assets/wall4.png"]
-DEFAULT_MAP_FILE = "maps/custom_map.json"
